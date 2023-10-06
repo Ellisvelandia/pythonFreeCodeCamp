8ff5209a1412b8276bb9f61630e580f197865993
Ellis velandia <eyis619@gmail.com>
2023-10-05 19:34:01 -0500

    first-steps

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-player_choice = "rock"
-print(player_choice)
+def get_choices():
+    player_choice = "rock"
+    computer_choice = "paper"
+    print(player_choice)
+    return computer_choice
+
+def greeting():
+    return "Hi"
+
+response = greeting()
+print(response)
