030fdbe669b5c299fe04143fc2b539feceba4be9
Ellis velandia <eyis619@gmail.com>
2023-10-06 21:37:46 -0500

    expressiones

--- expressions.py
+++ expressions.py
@@ -1,3 +1,24 @@
 # a variable is a named place in the memory where a programmer can store data and later retrieve the data using the variable "name"
 
 # programmers get to choose the names of the variables
+
+width = 15
+height = 12.0
+print(height / 3)
+
+# + Addition
+# - Substaction
+# * Multiplication
+# / Division
+# ** Power
+# % Remainder
+
+# Paranthesis
+# power
+# Multiplicatiion
+# Addtion
+# left to right
+
+inp = input("Europe floor?")
+usf = int(inp) + 1
+print("US floor", usf)
