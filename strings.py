79831259b512895e45f213f0f963ebfba1c176a9
Ellis velandia <eyis619@gmail.com>
2023-10-10 22:06:28 -0500

    strings

---
+++ strings.py
@@ -0,0 +1,7 @@
+# Strings DATA TYPE
+
+# A string is a sequence of characters
+# A string literal uses quotes "HELLO" or "Hello"
+# For strings , + means "concatenate"
+# when a string contains numbers, it is still a string
+# we can convert numbers in a string into a using int()
